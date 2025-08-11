35524b46cc173e680eac1c8610e13c4b35dedcae
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-08-11 17:56:41 +0530

    feat: update usernames controller to fetch and insert usernames from the database

--- controllers/usernamesController.js
+++ controllers/usernamesController.js
@@ -1,12 +1,16 @@
-exports.usernamesGet = (req, res) => {
-  console.log("usernames will be logged here - wip");
-  res.send("all usernames");
+const db = require("../db/query");
+
+exports.usernamesGet = async (req, res) => {
+  const usernames = await db.getAllUsernames();
+  res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
 };
 
 exports.usernamesNewGet = (req, res) => {
   res.render("addUsername", { title: "Add new Username" });
 };
 
-exports.usernamesNewPost = (req, res) => {
-  console.log("username to be saved: ", req.body.username);
+exports.usernamesNewPost = async (req, res) => {
+  const { username } = req.body;
+  await db.insertUsername(username);
+  res.redirect("/");
 };
